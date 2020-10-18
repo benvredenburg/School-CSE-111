457ec073f430bdc0ddbd9aace84dc0043940116d
benvredenburg <vre19001@byui.edu>
2020-10-17 22:23:30 -0600

    10/17

---
+++ test.py
@@ -0,0 +1,9 @@
+import math
+def torus_volume(inner, outer):
+    """Compute and return the volume of a torus."""
+    vol = 2 * (math.pi * inner) ** 2 * outer
+    return vol
+
+answer = torus_volume(1, 10)
+
+print(answer)
