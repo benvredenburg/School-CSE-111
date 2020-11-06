a90582a85e75c6f70f6af164b2c18ed04db357db
benvredenburg <vre19001@byui.edu>
2020-11-06 12:30:35 -0700

    11/06

--- CSE 111 - loops.py
+++
@@ -1,8 +0,0 @@
-# Write a loop that will put the integers between 5-10 into a list.
-
-numbers = []
-
-for i in range(5, 11):
-    numbers.append(i)
-    
-print(numbers)
---
+++ W03/CSE 111 - W03 Team Activity.py
@@ -0,0 +1,37 @@
+# Import Modules.
+
+import random
+import textwrap
+
+# Open text file.
+
+with open('W03quotes.txt', 'rt') as infile:
+
+    # Read quotes within file.
+
+    quotes = infile.readlines()
+
+    # Request number of quotes from user.
+
+num = int(input('How many quotes would you like? '))
+
+    # Display requested quotes to user.
+
+for _ in range(num):
+        
+        # Choose a quote at random.
+
+    quote = random.choice(quotes)
+
+        # Wrap quotes displayed to user.
+
+    wrapped = textwrap.wrap(quote, 60)
+
+        # Line break.
+
+    print()
+
+        # Display quote(s) to user.
+        
+    for line in wrapped:
+        print(line)
