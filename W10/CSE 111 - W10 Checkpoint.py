e3a2867f917b467877019015111e792908bf58b6
benvredenburg <vre19001@byui.edu>
2020-11-15 20:04:33 -0700

    11/15

---
+++ W10/CSE 111 - W10 Checkpoint.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def main():
+    try:
+        print()
+        df = pd.read_csv('W09water.csv', 
+                dtype={"meterNumber":"str", "meterSize":"float32",
+                    "readDate":"str", "numberOfDays":"int_", "usage":"int_",
+                    "accountType":"str", "numberOfDwellings":"int_"
+                }, 
+                parse_dates=['readDate'])
+
+        df['readDate'] = df['readDate'].dt.date
+        
+        meter = 'M4103'
+        df = filter_for_meter(df, meter)
+
+        # Define a vertical bar plot from the DataFrame.
+        barplot = df.plot.bar(x='readDate', y='usage', legend=False)
+        barplot.set_title(f"Water Usage for Meter #{meter}")
+        barplot.set_xlabel("")
+        barplot.set_ylabel("x1000 gallons")
+        plt.tight_layout()
+
+        # Draw and show all defined plots.
+        plt.show()
+
+    except RuntimeError as ex:
+        print(type(ex).__name__, ex, sep=": ")
+
+
+def filter_for_meter(df, meter):
+    """Return a new DataFrame that contains only the rows
+    where the meterNumber equals the parameter meter.
+    """
+    meter_filter = (df['meterNumber'] == meter)
+    meter_filtered = df[meter_filter]
+    df = meter_filtered
+    return df
+
+
+
+
+
+main()
--- test.py
+++
@@ -1,4 +0,0 @@
-met_num = input('Enter a meter number: ')
-met_num_actual = 'M' + met_num
-
-print(met_num_actual)
