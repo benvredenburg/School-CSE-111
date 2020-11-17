82eb9408fd736a71a9d96ed58c771097e32c4d4f
benvredenburg <vre19001@byui.edu>
2020-11-16 19:19:29 -0700

    11/16

--- W10/meter_usage.py
+++ W10/meter_usage.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 
 
@@ -11,37 +12,100 @@ def main():
         # Add a yearMonth column to the DataFrame.
         df = add_year_month_column(df)
         
+        # Add a medianUsage column to the DataFrame.
+        df = add_median_usage_column(df)
 
-        # Add a medianUsage column to the DataFrame
-
-        # Repeat as necessary.
+        # Repeat as necessary.     
 
         # Get a meter number from the user.
-        meter = input('Please enter a meter number: ')
-        df = filter_for_meter(df, meter)
+        meter = input('Please enter a meter number ("q" to quit): ').capitalize()
         
-        
-        # Get start and end years from the user.
-        print('Please enter dates in either YYYY-MM-DD, YYYY-MM, or YYYY format:')
-        print()
+
+        ''' 
+        If the user ever wants to pull dates by "YYYY/MM/DD" format, they can replace this section with the following
+        lines of code:
+
         start = pd.to_datetime(input('enter start date: '))
         end = pd.to_datetime(input('enter end date: '))
-        df = filter_between_dates(df, start, end)
-        print()
-        
+
+        continue to section marker below
+        '''
+
+        # Get start and end years from the user.
+        start_year = get_int('Please enter a starting year between 2015 and 2019, inclusive: ', 2015, 2019)
+        end_year = get_int(f'Please enter a starting year between {start_year} and 2019, inclusive: ', start_year, 2019)
+
         # Convert the start and end years
         # from integers to date strings.
+        start = pd.to_datetime(start_year, format='%Y')
+        end = pd.to_datetime(end_year + 1, format='%Y')   
 
+        ''' 
+        End of section from above lines of comments
+        '''   
+        
         # Filter the DataFrame to the meter number
-        # and years specifified by the user.        
-
-        # Define two plots.
-
-        # Show all defined plots.
+        # and years specifified by the user. 
+        df = filter_between_dates(df, start, end)      
+        df = filter_for_meter(df, meter)   
 
         print(df)
 
-        
+        # Define two plots.
+        show_meter_usage(df, meter)
+
+        # Show all defined plots.
+        show_comparison(df, meter)
+
+        plt.show()
+        print('Goodbye')
+
+        # meter = None
+        # while meter != 'Quit':
+        #     # Repeat as necessary.     
+
+        #     # Get a meter number from the user.
+        #     meter = input('Please enter a meter number ("q" to quit): ').capitalize()
+            
+
+        #     ''' 
+        #     If the user ever wants to pull dates by "YYYY/MM/DD" format, they can replace this section with the following
+        #     lines of code:
+
+        #     start = pd.to_datetime(input('enter start date: '))
+        #     end = pd.to_datetime(input('enter end date: '))
+
+        #     continue to section marker below
+        #     '''
+        #     if meter != 'Quit':
+        #         # Get start and end years from the user.
+        #         start_year = get_int('Please enter a starting year between 2015 and 2019, inclusive: ', 2015, 2019)
+        #         end_year = get_int(f'Please enter a starting year between {start_year} and 2019, inclusive: ', start_year, 2019)
+
+        #         # Convert the start and end years
+        #         # from integers to date strings.
+        #         start = pd.to_datetime(start_year, format='%Y')
+        #         end = pd.to_datetime(end_year + 1, format='%Y')   
+
+        #         ''' 
+        #         End of section from above lines of comments
+        #         '''   
+                
+        #         # Filter the DataFrame to the meter number
+        #         # and years specifified by the user. 
+        #         df = filter_between_dates(df, start, end)      
+        #         df = filter_for_meter(df, meter)   
+
+        #         print(df)
+
+        #         # Define two plots.
+        #         show_meter_usage(df, meter)
+
+        #         # Show all defined plots.
+        #         show_comparison(df, meter)
+
+        #         plt.show()
+        # print('Goodbye')
        
     except RuntimeError as ex:
         print(type(ex).__name__, ex, sep=": ")
@@ -58,7 +122,20 @@ def get_int(prompt, lower, upper):
     param lower: The lowest (smallest) integer that the user may enter.
     param upper: The highest (largest) integer that the user may enter.
     """
-    pass
+    while True:
+        try:
+            integer = int(input(prompt))
+            if integer < lower:
+                print(f'{integer} is too low. Please enter another integer.')
+            elif integer > upper:
+                print(f'{integer} is too high. Please enter another integer.')
+            else:
+                break
+        except ValueError as _:
+            # print(ex)
+            # print()
+            print('Invalid character in integer')
+    return integer
 
 
 def insert_after(alist, existing, toinsert):
@@ -69,7 +146,9 @@ def insert_after(alist, existing, toinsert):
     param toinsert: the element that this function will insert into
         alist after the existing element.
     """
-    pass
+    location = alist.index(existing)
+    alist.insert(location + 1, toinsert)
+    return alist
 
 
 def add_median_usage_column(df):
@@ -77,34 +156,33 @@ def add_median_usage_column(df):
     contains the median usage grouped by accountType and yearMonth.
     Return the new DataFrame.
     """
+    # unneccesary for current assignment as add_year_month() is already in main.
     # df = add_year_month_column(df)
-    # columns = df.columns.tolist()
+    columns = df.columns.tolist()
 
-    # # Find the median usage grouped by accountType and yearMonth.
-    # median_df = df.groupby(["accountType", "yearMonth"]) \
-    #         .aggregate(medianUsage=("usage", "median"))
+    # Find the median usage grouped by accountType and yearMonth.
+    median_df = df.groupby(["accountType", "yearMonth"]) \
+            .aggregate(medianUsage=("usage", "median"))
 
-    # # Change the index so that joining the median_df
-    # # with the original data frame will work.
-    # df.set_index(["accountType", "yearMonth"], inplace=True)
+    # Change the index so that joining the median_df
+    # with the original data frame will work.
+    df.set_index(["accountType", "yearMonth"], inplace=True)
 
-    # # Join the original data frame and the median data frame.
-    # joined_df = df.join(median_df)
+    # Join the original data frame and the median data frame.
+    joined_df = df.join(median_df)
 
-    # # The join sorts the data frame by the join key, which is
-    # # a different order than the CSV file, so sort the rows by
-    # # meterNumber and readDate like the CSV file, and then reset
-    # # the index as if the data were just read from the CSV file.
-    # joined_df.sort_values(["meterNumber", "readDate"], inplace=True)
-    # joined_df.reset_index(drop=False, inplace=True)
+    # The join sorts the data frame by the join key, which is
+    # a different order than the CSV file, so sort the rows by
+    # meterNumber and readDate like the CSV file, and then reset
+    # the index as if the data were just read from the CSV file.
+    joined_df.sort_values(["meterNumber", "readDate"], inplace=True)
+    joined_df.reset_index(drop=False, inplace=True)
 
-    # # Reorder the columns to be similar to the CSV file.
-    # insert_after(columns, "usage", "medianUsage")
-    # joined_df = joined_df[columns]
+    # Reorder the columns to be similar to the CSV file.
+    insert_after(columns, "usage", "medianUsage")
+    joined_df = joined_df[columns]
 
-    # return joined_df
-
-    pass
+    return joined_df
 
 
 def add_year_month_column(df):
@@ -126,7 +204,6 @@ def filter_for_meter(df, meter):
     return df
 
 
-
 def filter_between_dates(df, start, end):
     """Return a new DataFrame that contains only the rows where
     the readDate is between the specified start and end dates.
@@ -135,8 +212,7 @@ def filter_between_dates(df, start, end):
     date_range_filter = (df['readDate'] >= start) & (df['readDate'] <= end)
     date_filter = df[date_range_filter]
     df = date_filter
-    return df
-    
+    return df 
 
 
 def show_meter_usage(df, meter):
@@ -148,7 +224,12 @@ def show_meter_usage(df, meter):
         filtered to the rows for one meter number only.
     param meter: the meter number for which df is filtered.
     """
-    pass
+    
+    barplot = df.plot.bar(x='yearMonth', y='usage', legend=False)
+    barplot.set_title(f"Water Usage for Meter #{meter}")
+    barplot.set_xlabel("")
+    barplot.set_ylabel("x1000 gallons")
+    plt.tight_layout()
 
 
 def show_comparison(df, meter):
@@ -160,8 +241,12 @@ def show_comparison(df, meter):
         filtered to the rows for one meter number only.
     param meter: the meter number for which df is filtered.
     """
-    pass
-
+    plot = df.plot.line(x='yearMonth', y=['usage', 'medianUsage'])
+    plot.set_title(f"Water Usage and Median Water usage for Meter #{meter}")
+    plot.set_xlabel("")
+    plot.set_ylabel("x1000 gallons")
+    plt.tight_layout()
+    plot.legend(['usage', 'median'])
 
 # Call the main function so that
 # this program will start executing.
