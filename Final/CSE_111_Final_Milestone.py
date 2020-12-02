846d11b26b503efd6ee09a21d5d7bccca1fb7e0f
benvredenburg <vre19001@byui.edu>
2020-12-02 13:35:44 -0700

    12/02

--- Final/CSE_111_Final_Milestone.py
+++ Final/CSE_111_Final_Milestone.py
@@ -1,35 +1,38 @@
 import pandas as pd
-import matplotlib as pyplot
+import matplotlib.pyplot as plt
 
 def main():
     try:
+
+
         # Create a line break between the terminal and the program.
         print()
 
         # Read the finalalarms.csv file and convert it to a dataframe.
         df = pd.read_csv('finalalarms.csv')
 
+        # Convert date column to datetime64.
+        df['date'] = pd.to_datetime(df['date'])
+        # df['building'] = df.astype({'building': int})
+        df['building'] = df['building'].astype(int)
 
-        # Add column for segment based on building number.
-        # df = add_segment_column(df)
-
-        # Add column for SLA based on alarm point, type, and time to clear(?).
-        # df = add_sla_column(df)
-
-        # Filter by segment(?).
-        # seg_df = filter_by_segment(df)
-
+        # Filter by date(?). 
+        '''Always necessary'''
+        # date_df = filter_by_date(df)
+        
         # Ask user for a building to show.
         building = int(input('which building would you like to show? '))
 
         # Filter by building(?).
         bldg_df = filter_by_building(df, building)
+        
+        # Ask user for an operator to show.
+        operator = input('Which operator would you like to see? ').capitalize()
 
-        # Filter by operator(?).
-        # op_df = filter_by_operator(df)
+        # Filter by operator.
+        op_df = filter_by_operator(df, operator)
 
-        # Filter by date(?).
-        # date_df = filter_by_date(df)
+        
 
         # Filter by alarm type(?).
         # type_df = filter_by_type(df)
@@ -40,34 +43,30 @@ def main():
 
 
         # show_alarms_by_date(date_df, )
-        # show_alarms_by_operator(op_df, )
+        show_alarms_by_operator(op_df, operator)
         # show_alarms_by_segment(seg_df, )
-        # show_alarms_by_building(bldg_df, )
+        show_alarms_by_building(bldg_df, building)
         # show_alarms_by_type(type_df, )
         # show_alarms_by_sla(sla_df, )
-        
+
+
+        plt.show()
+        # print(df)
         print(bldg_df)
+        print(op_df)
+        # print(df.dtypes)
+
+
+        
     except RuntimeError as ex:
         print(type(ex).__name__, ex, sep=": ")
 
-def add_segment_column():
-    # Add a column to the data frame that uses the building
-    # number in the 'building' column. Buildings 1 - 100 should
-    # be labeled as AMER, 101 - 200 should be labeled as EMEA,
-    # and buildings 201 - 300 should be labeled as APAC.
-    pass
 
-def add_sla_column():
-    # Add a column to the data frome the compares the alarm point
-    # with the time to clear to determine if the cell gets a pass
-    # or fail label. Server room has an SLA of 5:00, everything else
-    # has an SLA of 10:00.
-    pass
 
-def filter_by_segment(df, segment):
-    # Filter the data frame to only show alarms from a specific
-    # segment.
-    pass
+
+
+
+
 
 def filter_by_building(df, building):
     # Filter the data frame to only show alarms from a specific
@@ -82,7 +81,12 @@ def filter_by_building(df, building):
 def filter_by_operator(df, operator):
     # Filter the data frame to only show alarms from a specific
     # operator.
-    pass
+    name = operator
+    op_filter = (df['operator'] == name)
+    op_filtered = df[op_filter]
+    df = op_filtered
+    return df
+    # pass
 
 def filter_by_date(df, start, end):
     # Filter the data frame to only show alarms from a specific
@@ -94,9 +98,31 @@ def filter_by_type(df, alarmType):
     # alarm type.
     pass
 
-def filter_by_sla(df, sla):
-    # Filter the data frame to only show alarms from a specific
-    # that failed or passed SLA.
+
+
+def show_alarms_by_building(bldg_df, building):
+    
+    # barplot = bldg_df.plot.bar(x='date', legend=False)
+    # barplot.set_title(f'Number of alarms for building #{building}')
+    # barplot.set_xlabel('')
+    
+    # plt.tight_layout()
+
+    plot = bldg_df.plot.line(x='date')
+    plot.set_title(f"Number of alarms for building #{building}")
+    plot.set_xlabel("")
+    plt.tight_layout()
+
+    # pass
+def show_alarms_by_operator(op_df, operator):
+    plot = op_df.plot.line(x='date')
+    plot.set_title(f"Number of alarms for operator: {operator}")
+    plot.set_xlabel("")
+    plt.tight_layout()
+    # pass
+def show_alarms_by_type():
+    pass
+def show_alarms_by_date():
     pass
 
 main()
--- Final/CSE_111_Final_Milestone_Master.py
+++ Final/CSE_111_Final_Milestone_Master.py
@@ -1,29 +1,34 @@
 import pandas as pd
-import matplotlib as pyplot
+import matplotlib.pyplot as plt
 
 def main():
     try:
+
+
         # Create a line break between the terminal and the program.
         print()
 
         # Read the finalalarms.csv file and convert it to a dataframe.
         df = pd.read_csv('finalalarms.csv')
 
+        # Convert date column to datetime64.
+        df['date'] = pd.to_datetime(df['date'])
+        # df['building'] = df.astype({'building': int})
+        df['building'] = df['building'].astype(int)
+
 
         # Add column for segment based on building number.
         # df = add_segment_column(df)
 
-        # Add column for SLA based on alarm point, type, and time to clear(?).
-        # df = add_sla_column(df)
 
         # Filter by segment(?).
         # seg_df = filter_by_segment(df)
 
         # Ask user for a building to show.
-        building = int(input('which building would you like to show? '))
+        # building = int(input('which building would you like to show? '))
 
         # Filter by building(?).
-        bldg_df = filter_by_building(df, building)
+        # bldg_df = filter_by_building(df, building)
 
         # Filter by operator(?).
         # op_df = filter_by_operator(df)
@@ -42,27 +47,33 @@ def main():
         # show_alarms_by_date(date_df, )
         # show_alarms_by_operator(op_df, )
         # show_alarms_by_segment(seg_df, )
-        # show_alarms_by_building(bldg_df, )
+        # show_alarms_by_building(bldg_df, building)
         # show_alarms_by_type(type_df, )
         # show_alarms_by_sla(sla_df, )
+
+
+        # plt.show()
+        print(df)
+        # print(bldg_df)
+        print(df.dtypes)
+
+
         
-        print(bldg_df)
     except RuntimeError as ex:
         print(type(ex).__name__, ex, sep=": ")
 
-def add_segment_column():
+
+def add_segment_column(df):
     # Add a column to the data frame that uses the building
     # number in the 'building' column. Buildings 1 - 100 should
     # be labeled as AMER, 101 - 200 should be labeled as EMEA,
     # and buildings 201 - 300 should be labeled as APAC.
+   
+    # df['segment'] = ['value1', 'value2', 'value3']
+    # return df
     pass
 
-def add_sla_column():
-    # Add a column to the data frome the compares the alarm point
-    # with the time to clear to determine if the cell gets a pass
-    # or fail label. Server room has an SLA of 5:00, everything else
-    # has an SLA of 10:00.
-    pass
+
 
 def filter_by_segment(df, segment):
     # Filter the data frame to only show alarms from a specific
@@ -94,9 +105,27 @@ def filter_by_type(df, alarmType):
     # alarm type.
     pass
 
-def filter_by_sla(df, sla):
-    # Filter the data frame to only show alarms from a specific
-    # that failed or passed SLA.
+
+
+def show_alarms_by_building(bldg_df, building):
+    
+    # barplot = bldg_df.plot.bar(x='date', legend=False)
+    # barplot.set_title(f'Number of alarms for building #{building}')
+    # barplot.set_xlabel('')
+    
+    # plt.tight_layout()
+
+    plot = bldg_df.plot.line(x='date')
+    plot.set_title(f"Number of alarms for building #{building}")
+    plot.set_xlabel("")
+    plt.tight_layout()
+
+    # pass
+def show_alarms_by_operator():
+    pass
+def show_alarms_by_type():
+    pass
+def show_alarms_by_date():
     pass
 
 main()
