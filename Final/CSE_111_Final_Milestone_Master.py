97aea43b8ddc6f6934553ddffd6ff9d82d7b88b0
benvredenburg <vre19001@byui.edu>
2020-12-10 19:22:06 -0700

    12/10

--- Final/CSE_111_Final_Milestone_Master.py
+++ Final/CSE_111_Final_Milestone_Master.py
@@ -10,82 +10,78 @@ def main():
         print()
 
         # Read the finalalarms.csv file and convert it to a dataframe.
-        df = pd.read_csv('Final/finalalarms.csv', parse_dates=['date'])
+        df = pd.read_csv('Final/finalalarms.csv')
 
-        # Convert date column to datetime64.
-        # df['date'] = pd.to_datetime(df['date']).dt.date
+        # Convert date column to datetime64 and building column into int.
+        df['date'] = pd.to_datetime(df['date']).dt.date
         df['building'] = df['building'].astype(int)
 
-        # Filter by date range. 
-
+         
         # Ask user for a date range.
-
         print('Please do not choose a date range over one week.')
         print()
         start = pd.to_datetime(input('enter start date in YYYY-MM-DD format: '))
         end = pd.to_datetime(input('enter end date in YYYY-MM-DD format: '))
         print()
 
-        # Convert the start and end years
-        # from integers to date strings.
-        start = pd.to_datetime(start)
-        end = pd.to_datetime(end)   
-
+        # Filter by provided date range. 
         df = filter_between_dates(df, start, end)
 
-        
+        # Create loop for adding any additional filters.
         choice1 = None
-
         while True:
             choice1 = input('Would you like to add a filter? Please enter (N) for no, (Y) for yes, or (Q) to quit: ').capitalize()
+            print()
 
             if choice1 == 'Y':
                 print("Filter options are:  OPERATOR,  BUILDING number, and  alarm TYPE. ")
                 filter_choice = input('Which filter would you like to apply? ').capitalize()
+                print()
                 if filter_choice == 'Building':
 
                     # Ask user for a building to show.
-                    building = int(input('which building would you like to show? '))
+                    building = int(input('which building (1 - 300) would you like to show? '))
 
-                    # Filter by building(?).
+                    # Filter by building.
                     df = filter_by_building(df, building)
 
                 elif filter_choice == 'Operator':
             
                     # Ask user for an operator to show.
-                    operator = input('Which operator would you like to see? ').capitalize()
+                    operator = input('Which operator would you like to see?\nAustin\nBenjamin\nJoseph\nMark\n').capitalize()
 
                     # Filter by operator.
                     df = filter_by_operator(df, operator)
 
                 elif filter_choice == 'Type':
-                    # Filter by alarm type(?).
-                    # df = filter_by_type(df)
-                    alarmType = input('Which alarm type would you like to see? ').upper()
 
+                    # Ask user for an alarm type.
+                    alarmType = input('Which alarm type would you like to see?\n\nDoor Forced\nFire\nServer\n').upper()
+
+                    # Filter by alarm type.
                     df = filter_by_type(df, alarmType)
-
-                    # pass
-                    
+            
+            # Create a grouping a the number of alarms for each day of the date range
+            # for the building, operator, and/or alarm type the user filtered by.
             elif choice1 == 'N':
                 grouped_df = group_by(df)
-                
                 break
-
+            
+            # End program if user chooses to quit.
             elif choice1 == 'Q':
                 print('Have a nice day!')
                 break
             
+            # Inform the user that they chose an incorrect choice at beginning of loop.
             else:
                 print('Invalid choice.')
-            
         
-
+        # Display the dataframe, number of alarms for applied filters by day, and a graph
+        # of the information in the latter.
         print(df)
         print(grouped_df)
         show_alarms_by_date(grouped_df)
         plt.show()
-        # print(df.dtypes)
         
     except RuntimeError as ex:
         print(type(ex).__name__, ex, sep=": ")
@@ -102,28 +98,22 @@ def filter_between_dates(df, start, end):
     return df 
 
 def filter_by_building(df, building):
-    # Filter the data frame to only show alarms from a specific
-    # building.
+    
     bldgNum = building
     bldg_filter = (df['building'] == bldgNum)
     bldg_filtered = df[bldg_filter]
     df = bldg_filtered
     return df
-    # pass
 
 def filter_by_operator(df, operator):
-    # Filter the data frame to only show alarms from a specific
-    # operator.
+
     name = operator
     op_filter = (df['operator'] == name)
     op_filtered = df[op_filter]
     df = op_filtered
     return df
-    # pass
 
 def filter_by_type(df, alarmType):
-    # Filter the data frame to only show alarms from a specific
-    # alarm type.
 
     alarmType = alarmType
     type_filter = (df['alarmType'] == alarmType)
@@ -131,22 +121,16 @@ def filter_by_type(df, alarmType):
     df = type_filtered
     return df
 
-    # pass
-
 def group_by(df):
     group = df.groupby("date")
-    df = group.aggregate(count=("date", "count"))
+    df = group.aggregate(alarms=("date", "count"))
     return df
 
 def show_alarms_by_date(grouped_df):
-    counts = list(grouped_df["count"])
-    # print(counts)
 
-    barplot = grouped_df.plot.bar(y="count", legend=False)
-    barplot.set_title('Number of alarms by date: ')
-    barplot.set_xlabel(counts,
-              loc="center")
-        
+    counts = list(grouped_df['alarms'])
+    barplot = grouped_df.plot.bar(y="alarms", legend=False)
+    barplot.set_title(f'Number of alarms by date:\n{counts}')
     plt.tight_layout()
 
 if __name__ == "__main__":
--- Final/final_test.py
+++ Final/final_test.py
@@ -4,60 +4,42 @@ from CSE_111_Final_Milestone_Master import filter_between_dates, filter_by_build
 
 def test_between_dates():
     
-    df = pd.read_csv('finalalarms.csv', parse_dates=['date'])
-    
-    # assert df['date'] == date
-    start = pd.to_datetime('2018-01-01')
-    end = pd.to_datetime('2018-01-07')
-    df = pd.DataFrame(filter_between_dates(df, start, end))
+    df = pd.read_csv('Final/finalalarms.csv')
+    start1 = '2018-01-01'
+    end1 = '2018-01-07'
+    df = pd.DataFrame(filter_between_dates(df, start1, end1))
     for row in df.iterrows():
-        assert row['date'] >= start and row['date'] < end
+        assert row['date'] >= start1 and row['date'] <= end1
 
 def test_filter_by_building():
 
-    df = pd.read_csv('finalalarms.csv', parse_dates=['date'])
-
+    df = pd.read_csv('Final/finalalarms.csv')
     building = '52'
     df = pd.DataFrame(filter_by_building(df, building))
-
     for row in df.iterrows():
         assert row['building'] == '52'
 
 def test_filter_by_operator():
-    df = pd.read_csv('finalalarms.csv', parse_dates=['date'])
+    df = pd.read_csv('Final/finalalarms.csv')
     operator = 'benjamin'
     df = pd.DataFrame(filter_by_building(df, operator))
-
     for row in df.iterrows():
         assert row['operator'] == 'benjamin'
 
-#     df = pd.read_csv('finalalarms.csv')
-#     operator = 'benjamin'
-#     for row in df.iterrows():
-#         assert operator == 'benjamin'
-
 def test_filter_by_type():
-    df = pd.read_csv('finalalarms.csv', parse_dates=['date'])
+    df = pd.read_csv('Final/finalalarms.csv')
     alarmType = 'fire'
     df = pd.DataFrame(filter_by_building(df, alarmType))
-
     for row in df.iterrows():
         assert row['alarmType'] == 'fire'
 
-#     df = pd.read_csv('finalalarms.csv')
-#     alarmType = 'fire'
-#     for row in df.iterrows():
-#         assert alarmType == alarmType
-
 def test_group_by():
-    df = pd.read_csv('finalalarms.csv', parse_dates=['date'])
-    start = pd.datetime('2018-01-01')
-    end = pd.datetime('2018-01-07')
+    df = pd.read_csv('Final/finalalarms.csv')
+    start = '2018-01-01'
+    end = '2018-01-07'
     df = pd.DataFrame(filter_between_dates(df, start, end))
-
     grouped_df = pd.DataFrame(group_by(df))
-
-    for row in df.iterrows():
+    for row in grouped_df.iterrows():
         assert row['count'] == '349'
 
 pytest.main(["Final/final_test.py"])
